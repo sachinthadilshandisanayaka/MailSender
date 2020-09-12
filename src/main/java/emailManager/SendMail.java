a10bc6fb822535f0fcde10af657dc1cf330b1c7d
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-12 15:55:36 +0530

    url change

--- src/main/java/emailManager/SendMail.java
+++ src/main/java/emailManager/SendMail.java
@@ -7,9 +7,11 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class SendMail {
     public JsonNode sendSimpleMessage(String email, String erroMessage) throws UnirestException {
+        String url = null;
+        String password = null;
 
-        HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + "sandbox1578a10878f343abafdf04c79479b78b.mailgun.org" + "/messages")
-                .basicAuth("api", "1f39a178b73a57ce04dee76361879a98-0f472795-e19a09a9")
+        HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + url + "/messages")
+                .basicAuth("api", password)
                 .field("from", "sachidisanayaka98@gmail.com")
                 .field("to", email)
                 .field("subject", "ERROR")
